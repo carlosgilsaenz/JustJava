16f8fc0e8383245695ae82c5a7ff33d42e0710cd
John Doe <john.doe@example.com>
2017-02-01 23:59:19 -0800

    add cost for all topping add toast msg on decrement bug add toast msg on increment bug

--- src/main/java/com/example/android/justjava/MainActivity.java
+++ src/main/java/com/example/android/justjava/MainActivity.java
@@ -7,6 +7,7 @@
 
 package com.example.android.justjava;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,6 +15,7 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * This app displays an order form to order coffee.
@@ -22,7 +24,6 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     int quantity = 0;
-    int costPerCoffee = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,33 +34,57 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view){
-        int price = calculatePrice();
-        Log.v("MainActivity", "The price is " + price);
+
+        CheckBox whippedCream = (CheckBox)findViewById(R.id.whipped_cream_checkbox);
+        CheckBox chocolate = (CheckBox)findViewById(R.id.chocolate_checkbox);
+
+        boolean isWhippedCream = whippedCream.isChecked();
+        boolean isChocolate = chocolate.isChecked();
+
+        int price = calculatePrice(isWhippedCream, isChocolate);
 
         EditText editText= (EditText) findViewById(R.id.name_editText);
         String name = editText.getText().toString();
-        Log.v("MainActivity","The name entered is " + name);
 
-        displayMessage(createOrderSummary(price, name));
+        displayMessage(createOrderSummary(price, name, isWhippedCream, isChocolate));
     }
 
     /**
+     * @param addWhippedCream adds 1 to costPerCoffee if true
+     * @param addChocolate adds 2 to costPerCoff if true
      * @return total price
      */
-    private int calculatePrice(){
+    private int calculatePrice(boolean addWhippedCream, boolean addChocolate){
+        //cost per coffee
+        int costPerCoffee = 5;
+
+        //adds $1 if coffee is whipped
+        if(addWhippedCream){
+            costPerCoffee += 1;
+        }
+
+        //adds $2 if coffee has chocolate
+        if(addChocolate){
+            costPerCoffee += 2;
+        }
+
+        //returns total cost
         return quantity * costPerCoffee;
     }
 
     /**
      * Creates String for Order summary
      *
-     * @param price of the order
+     * @param price total order
+     * @param name customers name
+     * @param whippedCream determine if whipped
+     * @param chocolate determine if added chocolates
      * @return complete order summary in String form
      */
-    private String createOrderSummary(int price, String string){
-        String order = "Name: " + string;
-        order += "\nAdd Whipped cream? " + isChecked((CheckBox)findViewById(R.id.whipped_cream_checkbox));
-        order += "\nAdd Chocolate? " + isChecked((CheckBox)findViewById(R.id.chocolate_checkbox));
+    private String createOrderSummary(int price, String name, boolean whippedCream, boolean chocolate){
+        String order = "Name: " + name;
+        order += "\nAdd Whipped cream? " + whippedCream;
+        order += "\nAdd Chocolate? " + chocolate;
         order += "\nQuantity: " + quantity;
         order += "\nTotal: $"+ price + "\nThank You!";
         return order;
@@ -86,7 +111,16 @@ public class MainActivity extends AppCompatActivity {
      * @param view affects the Quantity display on screen
      */
     public void increment(View view){
-        quantity++;
+        if(quantity < 100) {
+            quantity++;
+        } else if(quantity == 100){
+            CharSequence text = "Cannot order more than 100 cups.";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(this, text, duration);
+            toast.show();
+        }
+
         display(quantity);
     }
 
@@ -95,12 +129,15 @@ public class MainActivity extends AppCompatActivity {
      */
     public void decrement(View view){
         if(quantity > 0){
-            quantity--;}
-        display(quantity);
-    }
+            quantity--;
+        } else if(quantity == 0){
+            CharSequence text = "Cannot order negative cups.";
+            int duration = Toast.LENGTH_SHORT;
 
-    private boolean isChecked(CheckBox checkBox){
-        return checkBox.isChecked();
+            //display error message
+            Toast.makeText(this, text, duration).show();
+        }
+        display(quantity);
     }
 }
 
