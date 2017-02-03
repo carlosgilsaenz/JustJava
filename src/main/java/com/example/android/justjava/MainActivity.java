1162e69e0ae7eaa2889f86947ee1605b768f23e6
John <JohnDoe@example.com>
2017-02-02 16:56:22 -0800

    add Intent.ACTION_SENDTO for email remove displayMessage() remove Summary within XML restructured submitOrder()

--- src/main/java/com/example/android/justjava/MainActivity.java
+++ src/main/java/com/example/android/justjava/MainActivity.java
@@ -8,6 +8,8 @@
 package com.example.android.justjava;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -43,13 +45,22 @@ public class MainActivity extends AppCompatActivity {
 
         int price = calculatePrice(isWhippedCream, isChocolate);
 
+        //retrieves name for order
         EditText editText= (EditText) findViewById(R.id.name_editText);
         String name = editText.getText().toString();
 
-        displayMessage(createOrderSummary(price, name, isWhippedCream, isChocolate));
+        //creates order summary string
+        String order = createOrderSummary(price, name, isWhippedCream, isChocolate);
+
+        //create subject line string
+        String subject = getApplicationName(this) + " order for " + name;
+
+        //creates intent for email
+        composeEmail(subject, order);
     }
 
     /**
+     * This method is called from from submitOrder to calculate price
      * @param addWhippedCream adds 1 to costPerCoffee if true
      * @param addChocolate adds 2 to costPerCoff if true
      * @return total price
@@ -82,6 +93,7 @@ public class MainActivity extends AppCompatActivity {
      * @return complete order summary in String form
      */
     private String createOrderSummary(int price, String name, boolean whippedCream, boolean chocolate){
+        //create string with multiple concatenations
         String order = "Name: " + name;
         order += "\nAdd Whipped cream? " + whippedCream;
         order += "\nAdd Chocolate? " + chocolate;
@@ -96,24 +108,19 @@ public class MainActivity extends AppCompatActivity {
     private void display(int number){
         TextView quantityView = (TextView) findViewById(R.id.quantity_text_view);
         quantityView.setText("" + number);
+        //old code below
         //quantityView.setText(NumberFormat.getInstance().format(number));
-    }
-
-    /**
-     * @param coffeeOrder String of complete order details
-     */
-    private void displayMessage(String coffeeOrder){
-        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
-        orderSummaryTextView.setText(coffeeOrder);
     }
 
     /**
      * @param view affects the Quantity display on screen
      */
     public void increment(View view){
+        //ensure quantity doesn't go over 100 orders
         if(quantity < 100) {
             quantity++;
         } else if(quantity == 100){
+            //setup toast message to notify user
             CharSequence text = "Cannot order more than 100 cups.";
             int duration = Toast.LENGTH_SHORT;
 
@@ -128,9 +135,11 @@ public class MainActivity extends AppCompatActivity {
      * @param view affects the Quantity display on screen
      */
     public void decrement(View view){
+        //ensure quantity doesn't go negative
         if(quantity > 0){
             quantity--;
         } else if(quantity == 0){
+            //setup toast message to notify user
             CharSequence text = "Cannot order negative cups.";
             int duration = Toast.LENGTH_SHORT;
 
@@ -138,6 +147,29 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, text, duration).show();
         }
         display(quantity);
+    }
+
+    /**
+     * Sends Order Summary via Email client
+     *@param order text of email
+     * @param subject text for subject order
+     */
+    public void composeEmail(String subject, String order) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); //only email apps should handle this
+        intent.putExtra(Intent.EXTRA_TEXT, order);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.v("composeEmail", "Failed to resolveActivity()");
+        }
+
+    }
+
+    public String getApplicationName(Context context) {
+        String appName = context.getApplicationInfo().loadLabel(getPackageManager()).toString();
+        return appName;
     }
 }
 
